430c629e477b92df4e847c1e96f7357270e2a052
FerJuan <fbatres@gmail.com>
2024-10-03 17:06:48 -0300

    agregando al header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import { Link, NavLink } from "react-router-dom"
+
 export const Header = () => {
   return (
 <header className="bg-slate-800">
@@ -6,8 +8,14 @@ export const Header = () => {
         <div>
             <img src="/logo.svg" className="w-32" alt="logo de Drinks" />
         </div>
-        <nav>
-
+        <nav className="flex gap-4">
+            <NavLink to="/"
+            className="text-white uppercase font-bold"
+            > 
+            Inicio </NavLink>
+            <Link to="/favoritos"
+            className="text-white uppercase font-bold"
+            >Favoritos</Link>
         </nav>
     </div>
 </div>
